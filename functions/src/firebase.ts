75464d0bff1d376fe8e85d3307061ccd42a7274d
hellokenta1024 <hellokenta1024@gmail.com>
2020-04-24 21:09:35 +0900

    Setup functions

---
+++ functions/src/firebase.ts
@@ -0,0 +1,7 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore;
+export { functions, admin, db };
